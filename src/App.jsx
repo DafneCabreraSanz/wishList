ba08a10ab527c37fca97d8372a1a8f5b2ae6e579
DafneCabreraSanz <alumno.712343@ies-azarquiel.es>
2025-12-10 10:55:40 +0100

    Dia 10/12/25

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import './App.css'
+import WishInput from './WishInput/WishInput.jsx'
+import WishList from './WishList/WishList';
+import { useState } from 'react';
 
-const wishes = [
+let initial_wishes = [
   { id: 0,
     text: 'Been millionaire', 
     completed: false },
@@ -13,37 +16,21 @@ const wishes = [
 ];
 
 function App() {
+ const [wishes, setWishes] = useState(initial_wishes);
+
+  function onNewWish(newWish){
+    //add the new wish to the wishes array
+    setWishes((currentWishes) => [...currentWishes, newWish]);
+  }
 
   return (
-    
       <div className = "app">
 
         <h1>My wishList</h1>
 
-        <fieldset className = "wish-input">
-          <legend className = "wish-input__label">New wish: </legend>
-          <input type="text" placeholder="My new wish" />
-        </fieldset>
-
-        <ul className="wish-list">
-          {wishes.map(wish => (
-          <li key={wish.id} 
-            className={`wish-list__item} 
-              ${wish.completed && 'wish-list__item--done'}
-            `}>
-
-            <input 
-              type="checkbox" 
-              checked={wish.completed} 
-              id={wish.id}
-             />
-
-            <label htmlFor={wish.id}> {wish.text} </label>
-
-          </li>
-          ))}
-        </ul>
-
+        <WishInput onNewWish={onNewWish}/>
+        <WishList wishes={wishes} />
+        
         <button type="button"> Archive all wishes </button>
 
       </div>
